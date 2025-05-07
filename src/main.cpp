a44f7fbfa4a6af0410850f3e6847324e0ed10fdd
Xeenych <xeenych@gmail.com>
2025-05-07 13:24:59 +0300

    add block size

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <cassert>
 #include <iostream>
+#include <sstream>
 
-int main() {
-   
+int main(int argc, char* argv[]) {
+    std::istringstream s{argv[1]};
+    int block_size = 0;
+    s >> block_size;
+
+    std::cout << "Block size is: " << block_size << std::endl;
+
     return 0;
 }
